a9b940662133ecdf2dfff03b946e66387bbbc301
crispycret <bnadeau.dev@gmail.com>
2022-09-20 09:23:54 -0700

    recent changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,27 @@ import './assets/css/App.css';
 
 import HomeSection from './components/HomeSection';
 import Navbar from './components/Navbar';
+import MyNavbar from './components/MyNavbar';
+import NavbarBasicExample from './components/Navbars/NavbarBasicExample';
+import NavbarBrandExample from './components/Navbars/NavbarBrandExample';
+import NavbarColorSchemesExample from './components/Navbars/NavbarColorSchemeExample';
+import NavbarTextLinkExample from './components/Navbars/NavbarTextLinkExample';
+import PortfolioNavbar from './components/Navbars/PorfolioNavbar';
 
 
 function App() {
   return (
     <div className="App">
-      {/* 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> 
-      */}
-        <Navbar />
-      <HomeSection />
+      {/* <MyNavbar /> */}
+      {/* <Navbar />
+      <NavbarBasicExample/>
+      <NavbarBrandExample />
+      <NavbarColorSchemesExample />
+      <NavbarTextLinkExample/>
+      <HomeSection /> */}
 
+
+      <PortfolioNavbar />
 
 {/* 
       <DemoSection />
